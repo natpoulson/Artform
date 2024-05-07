f130a17ae17652931c2aa6ae785def95c35174d2
natpoulson <nat.poulson@outlook.com.au>
2024-05-07 16:39:26 +1000

    Start preparing sidebar menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom"
-import Header from "./components/header";
+import Sidebar from "./components/sidebar";
 import Footer from "./components/footer";
 
 function App() {
 
   return (
     <>
-      <Header />
-        <main>
-          <Outlet />
-        </main>
-      <Footer />
+      <Sidebar />
+        <div>
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
     </>
   )
 }
